f29dc6ce99997438cdc9a307620e64cbef7ce5d2
Tanmay Kanase <157377290+Tanmay-kanase@users.noreply.github.com>
2025-07-18 11:25:37 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
   server: {
+    host: true, // allows binding to 0.0.0.0 (important for Render)
+    port: parseInt(process.env.PORT) || 5173, // use Render's PORT or default
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://road2tech-backend.onrender.com",
         secure: false,
       },
     },
