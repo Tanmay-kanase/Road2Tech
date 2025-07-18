4cf79256034bac0e805261cdd3d5031bef026356
Tanmay Kanase <157377290+Tanmay-kanase@users.noreply.github.com>
2025-07-18 11:19:09 +0530

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_BACKEND_URI,
   withCredentials: true, // optional: only if you use cookies/sessions
 });
 
