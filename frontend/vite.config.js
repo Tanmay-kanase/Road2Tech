a526d5ce2cd7fcf0f5e9cce083a5901656f8a6f3
Tanmay Kanase <tanmaykanase06@gmial.com>
2025-07-18 12:51:48 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  server: {
-    host: true,
-    port: 5173,
-    allowedHosts: ["road2tech.onrender.com"],
-    proxy: {
-      "/api": {
-        target: "https://road2tech-backend.onrender.com",
-        secure: false,
-      },
-    },
-  },
   plugins: [react(), tailwindcss()],
 });
