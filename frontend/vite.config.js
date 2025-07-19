7a3d6cfd2097295175f3b49154dbdbc572383319
Tanmay Kanase <157377290+Tanmay-kanase@users.noreply.github.com>
2025-07-19 21:32:19 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, // Optional for dev
+  }
 });
